4a3bbbe945259049f1e54f6796b571dfa1dfb3c7
Nicolas Facchinetti <facchinetti.nicolas@outlook.it>
2020-07-17 11:00:10 +0200

    modello con vincoli 1 - 2 - 3

--- src/main.jl
+++ src/main.jl
@@ -1,25 +1,31 @@
 include("tsp.jl")
 include("opt.jl")
 
+# parameters
+pck_k = 3                               # pickup veichle dimension
+dlv_k = 2                               # delivery veichle dimension
+file_dir = "./istanze/"                 # folder containing istances
+pck_file = "prova_p.tsp"                # pickup file
+dlv_file = "prova_d.tsp"                # delivery file
 
-file_dir = "./istanze/"
-pck_file = "33p00p.tsp"
-dlv_file = "33p00d.tsp"
 
+println("Starting...\nParse points files.")
 
-# parse the file for obtain the coords
+# parse the files for obtain the coords
 file_location = string(file_dir, pck_file)
 pck_points = parse_file(file_location)
 
 file_location = string(file_dir, dlv_file)
 dlv_points = parse_file(file_location)
 
+println("Compute distance matrix from points coords.")
+
 # get distance matrix from the points
 pck_matrix = get_distance_matrix(pck_points)
 dlv_matrix = get_distance_matrix(dlv_points)
 
-# get capacity of the two veichle
-pck_k = 3
-dlv_k = 2
+println("Get the base model of the problem.")
+model = build_model(pck_matrix, dlv_matrix)
 
-println("ciao")
+
+println("Exiting")
--- src/opt.jl
+++ src/opt.jl
@@ -4,55 +4,69 @@ using CPLEX
 
 
 function build_model(pck_matrix, dlv_matrix)
-    n = size(matrix, 1)                             # get dimension of the matrix (number nodes)
+    n = size(pck_matrix, 1)                         # get dimension of the matrix (number nodes)
     m = Model(CPLEX.Optimizer)                      # get a model with CPLEX as Optimizer
 
-    # _______________________ VARIABLES _________________________
+    # _____________________________________ VARIABLES _____________________________________
     # we have a variable x for every possible arc, 1 for pck, 2 for delivery
-    @variable(m, x1[n,n], Bin)                      # Bin for binary variables, n x n arcs
-    @variable(m, x2[n,n], Bin)                
-    
-
-    # Delta variables for counting the arcs outgoing and incoming from a node
-    # delta+ number of outgoing arcs:   δ+(S) = {(v, w) ∈ A: v ∈ S, w /∈ S}
-    # delta- number of incoming arcs:   δ-(S) = {(v, w) ∈ A: v /∈ S, w ∈ S}
-    @variable(m, delta-_pck[n])
-    @variable(m, delta+_pck[n])
-    @variable(m, delta-_dlv[n])
-    @variable(m, delta+_dlv[n])
+    @variables(m, begin
+        x1[1:n, 1:n], (Bin)                      
+        x2[1:n, 1:n], (Bin)
+    end)                           # Bin for binary variables, n x n arcs
 
     # cost of the two trips
-    @variable(m, cost_pck)
-    @variable(m, cost_dlv)
+    @variables(m, begin
+        cost_pck
+        cost_dlv
+    end)
 
-    # _______________________ OBJECTIVE _________________________
-    # Setting the objective
-    sum_trip = pck_cost + dlv_cost
+    # _____________________________________ OBJECTIVE _____________________________________
+    # Compute the objective as the sum of the two tour
+    sum_trip = @expression(m, cost_pck + cost_dlv)
     @objective(m, Min, sum_trip)
     
-    # _______________________ CONSTRAINT _________________________
+    
+    # _____________________________________ CONSTRAINT _____________________________________
     # Calculate the delta variables         v1 vertex 1, v2 vertex 2
-    @constraint(m, delta_pck-[v1 in 1:n], sum(x1[v2, v1] for v2 in 1:n) == delta-_pck[v1])
-    @constraint(m, delta_pck+[v1 in 1:n], sum(x1[v1, v2] for v2 in 1:n) == delta+_pck[v1])
-    @constraint(m, delta_dlv-[v1 in 1:n], sum(x2[v2, v1] for v2 in 1:n) == delta-_dlv[v1])
-    @constraint(m, delta_dlv+[v1 in 1:n], sum(x2[v1, v2] for v2 in 1:n) == delta+_dlv[v1])
+    # @constraint(m, delta_pck-[v1 in 1:n], sum(x1[v2, v1] for v2 in 1:n) == delta-_pck[v1])
+    # @constraint(m, delta_pck+[v1 in 1:n], sum(x1[v1, v2] for v2 in 1:n) == delta+_pck[v1])
+    # @constraint(m, delta_dlv-[v1 in 1:n], sum(x2[v2, v1] for v2 in 1:n) == delta-_dlv[v1])
+    # @constraint(m, delta_dlv+[v1 in 1:n], sum(x2[v1, v2] for v2 in 1:n) == delta+_dlv[v1])
     
     # constraint 1: outgoing archs = incoming archs from source: x_t(δ+(0)) = x_t(δ−(0)) t = 1, 2
-    @constraint(m, c1_pck, x1[] * delta+_pck[0] == x1 * delta-_pck[0])
-    @constraint(m, c1_dlv, )
+    @constraint(m, c1_pck, sum(x1[1, v] for v in 1:n) == sum(x1[v, 1] for v in 1:n))
+    @constraint(m, c1_dlv, sum(x2[1, v] for v in 1:n) == sum(x2[v, 1] for v in 1:n))
 
-    # constraint 2: every vertex visited only once: x_t(δ+(v)) = 1 for v = 1, 2, . . . , n, t = 1, 2
-    @constraint(m, c2_pck[v in i:n], x1[] * delta+_pck[v] == 1)
-    @constraint(m, c2_dlv[v in i:n], x2[] * delta+_dlv[v] == 1)
+    # constraint 2: every vertex visited only once: x_t(δ+(v)) = 1 for v = 1, 2, ..., n, t = 1, 2
+    # delta+ number of outgoing arcs:   δ+(S) = {(v, w) ∈ A: v ∈ S, w /∈ S}
+    @constraint(m, c2_pck[v1 in 2:n], sum(x1[v1, v2] for v2 in 1:n) == 1)
+    @constraint(m, c2_dlv[v1 in 2:n], sum(x2[v1, v2] for v2 in 1:n) == 1)
 
-    # constraint 3: every vertex visited only once: x_t(δ+(v)) = 1 for v = 1, 2, . . . , n, t = 1, 2
-    @constraint(m, c3_pck[v in i:n], x1[] * delta-_pck[v] == 1)
-    @constraint(m, c3_dlv[v in i:n], x2[] * delta-_dlv[v] == 1)
+    # constraint 3: every vertex visited only once: x_t(δ-(v)) = 1 for v = 1, 2, ..., n, t = 1, 2
+    # delta- number of incoming arcs:   δ-(S) = {(v, w) ∈ A: v /∈ S, w ∈ S}    
+    @constraint(m, c3_pck[v2 in 2:n], sum(x1[v1, v2] for v1 in 1:n) == 1)
+    @constraint(m, c3_dlv[v2 in 2:n], sum(x2[v1, v2] for v1 in 1:n) == 1)
 
     # calculate cost of the tour for pck and dlv
-    @constraint(m, pckcost, pck_matrix * x1 .== cost_pck)
-    @constraint(m, dlvcost, dlv_matrix * x2 .== cost_dlv)
+    @constraint(m, pckcost, sum(pck_matrix[i,j] * x1[i,j] for i in 1:n, j in 1:n) == cost_pck)
+    @constraint(m, dlvcost, sum(dlv_matrix[i,j] * x2[i,j] for i in 1:n, j in 1:n) == cost_dlv)
     
+    # no self loop
+    @constraint(m, no_self_pck[i in 1:n], x1[i,i] == 0)
+    @constraint(m, no_self_dlv[i in 1:n], x2[i,i] == 0)
+
+
+    # Printing the prepared optimization model
+    print(m)
+
+    # Solving the optimization problem
+    optimize!(m)
+    println("Optimal tour solutions:")
+    println("Pck tour = ", JuMP.value(cost_pck))
+    println("Dlv tour = ", JuMP.value(cost_dlv))
+    println("X1 tour = ", JuMP.value.(x1))
+    println("X2 tour = ", JuMP.value.(x2))
+
     return m
 end
 
