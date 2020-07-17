c9f30672ea55a5f294474a767e7a924f5830c20c
Nicolas Facchinetti <facchinetti.nicolas@outlook.it>
2020-07-17 17:44:27 +0200

    add possibility to round the distances to integer

--- src/main.jl
+++ src/main.jl
@@ -7,7 +7,7 @@ dlv_k = 2                               # delivery veichle dimension
 file_dir = "./istanze/"                 # folder containing istances
 pck_file = "prova_p.tsp"                # pickup file
 dlv_file = "prova_d.tsp"                # delivery file
-
+to_round = true                         # round the value when find the eclidean dist
 
 println("Starting...\nParse points files.")
 
@@ -21,16 +21,20 @@ dlv_points = parse_file(file_location)
 println("Compute distance matrix from points coords.")
 
 # get distance matrix from the points
-pck_matrix = get_distance_matrix(pck_points)
-dlv_matrix = get_distance_matrix(dlv_points)
+pck_matrix = get_distance_matrix(pck_points, to_round)
+dlv_matrix = get_distance_matrix(dlv_points, to_round)
 
 println("Get the base model of the problem.")
 model = build_model(pck_matrix, dlv_matrix)
-try
-    ptour, dtour, x1, x2 = solve(model)
+
+
+ptour, dtour, x1, x2 = try
+    solve(model)
 catch e
     println(e.msg)
 end
 
+println(ptour)
+println(dtour)
 
 println("Exiting")
--- src/tsp.jl
+++ src/tsp.jl
@@ -19,14 +19,21 @@ function parse_file(file)
     return points
 end
 
-function get_distance_matrix(points)
+function get_distance_matrix(points, to_round)
     n = length(points)
-    matrix = Matrix{Float64}(undef, n, n)                                  # get a matrix of dimension n*n of Float
+    matrix = Matrix{Float64}(undef, n, n)                           # get a matrix of dimension n*n of Float
     for (i, x1) in enumerate(points)
         for (j, x2) in enumerate(points)
-            dist = sqrt((x1[1] - x2[1])^2 + (x1[2] - x2[2])^2)      # euclidean distance between x1 and x2
+            dist = euclidean_distance(x1, x2)                       # euclidean distance between x1 and x2
+            if to_round
+                dist = round(dist)
+            end
             matrix[i, j] = dist                                     # populate the row
         end
     end
     return matrix
+end
+
+function euclidean_distance(x, y)
+    return sqrt((x[1] - y[1])^2 + (x[2] - y[2])^2)
 end
